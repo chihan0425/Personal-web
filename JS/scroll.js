a984d9a31504e41db7a8f62862137f7fd3fe01e1
chihan0425 <anny52.kzps@gmail.com>
2019-05-07 01:20:56 +0800

    0507

---
+++ JS/scroll.js
@@ -0,0 +1,37 @@
+//  $(function() {
+//   $('a[href*="#"]:not([href="#"])').click(function() {
+//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//       var target = $(this.hash);
+//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//       if (target.length) {
+//         $('html, body').animate({
+//           scrollTop: target.offset().top
+//         }, 1000);
+//         return false;
+//       }
+//     }
+//   });
+// });
+
+ $(function() {
+  $('#H2,#H3,#H4,#H5,#H6').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+   $("#gotop,#gotop2").click(function(){
+      jQuery("html,body").animate({
+          scrollTop:0
+      },1000);
+  });
+
+});
+
